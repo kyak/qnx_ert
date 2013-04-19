cc47e176d364084d20d87db34798ab97046da414
kyak <bas@bmail.ru>
2013-04-19 17:04:19 +0400

    Add setup preference for board IP address

--- qnx_setup.m
+++ qnx_setup.m
@@ -8,5 +8,13 @@ addpath(fullfile(curpath, 'ext_mode'));
 addpath(fullfile(curpath, 'ext_mode', 'target'));
 savepath;
 
+% Set up preferences
+if ispref('qnx_ert')
+	rmpref('qnx_ert');
+end
+% Ask for Target IP
+tip = inputdlg('Enter Target Board IP address:');
+addpref('qnx_ert','TargetIP',tip{1});
+
 % Refresh customizations
 sl_refresh_customizations;
