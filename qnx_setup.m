9d5e42e558cd12e0790c4072f76323c9de7aeeb6
kyak <bas@bmail.ru>
2013-04-19 10:37:58 +0400

    Add some bits to qnx_setup.m

--- qnx_setup.m
+++ qnx_setup.m
@@ -1,2 +1,12 @@
-addpath(genpath(pwd));
+function qnx_setup()
+
+% Set up paths
+curpath = pwd;
+addpath(fullfile(curpath, 'qnx'));
+addpath(fullfile(curpath, 'demos'));
+addpath(fullfile(curpath, 'ext_mode'));
+addpath(fullfile(curpath, 'ext_mode', 'target'));
+savepath;
+
+% Refresh customizations
 sl_refresh_customizations;
